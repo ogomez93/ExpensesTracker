73f2187a3d60c5af830dbc9272d44a813fc96e02
Oswaldo Gomez <oswaldogomezsl@gmail.com>
2018-11-30 22:18:30 -0300

    Initial commit.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render( < App / > , document.getElementById('root'));
+
+const getUsers = async () => {
+  try {
+    let response = await fetch('http://localhost:3000/api/v1/users');
+    let users = await response.json();
+    console.log(users);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getUsers();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
